ac5f771d353fc6d5e9d0beae7a1e8f0b94abfb7a
MycroftBrooks <57303529+MycroftBrooks@users.noreply.github.com>
2021-11-27 15:12:00 +0300

    push

---
+++ src/lab21/CreateTextDocument.java
@@ -0,0 +1,12 @@
+package lab21;
+
+public class CreateTextDocument implements ICreateDocument {
+    public IDocument CreateNew() {
+        TextDocument textDocument = new TextDocument();
+        return textDocument;
+    }
+    public IDocument CreateOpen() {
+        TextDocument textDocument = new TextDocument();
+        return textDocument;
+    }
+}
